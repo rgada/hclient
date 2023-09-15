24989d5b841904840a2bd0f980c6068a4a6bbcb5
Prateek Singh <prateek.singh@cloudera.com>
2023-09-15 16:54:48 +0530

    dbe xists fix

--- metastore-benchmarks/src/main/java/org/apache/hadoop/hive/metastore/tools/BenchmarkTool.java
+++ metastore-benchmarks/src/main/java/org/apache/hadoop/hive/metastore/tools/BenchmarkTool.java
@@ -223,8 +223,15 @@ public class BenchmarkTool implements Runnable {
 
     try (HMSClient client = new HMSClient(getServerUri(host, String.valueOf(port)), confDir)) {
       bData.setClient(client);
-      if (!client.dbExists(dbName)) {
-        client.createDatabase(dbName);
+
+      try{
+        if (!client.dbExists(dbName)) {
+          client.createDatabase(dbName);
+        }
+      }
+      catch (Exception e) {
+        e.printStackTrace();
+        dbName = dbName + "Dummy";
       }
 
       if (client.tableExists(dbName, tableName)) {
