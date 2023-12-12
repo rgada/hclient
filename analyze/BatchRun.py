69ba7421896e71d6d52383a8260f39a48479b01a
Prateek Singh <prateek.singh@cloudera.com>
2023-12-12 12:01:30 +0530

    spin count to 100

--- analyze/BatchRun.py
+++ analyze/BatchRun.py
@@ -22,7 +22,7 @@ import time
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-DEFAULT_SPIN = 500
+DEFAULT_SPIN = 100
 SPIN_COUNT = "--spin"
 COMMAND_DELIMITER = ";"
 
